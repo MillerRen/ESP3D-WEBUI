5b74a7ed2ca8facef3822c91a8a23cf1c8399642
MillerRen <ren12345@126.com>
2021-10-21 19:47:22 +0800

    move files

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,12 @@
 import API from "./apis"
 
 import Navbar from "./components/Navbar.vue"
-import Tabs from './components/Tabs.vue'
-
+import Tabs from './components/Tabs/Tabs.vue'
 import SettingsPanel from "./components/Tabs/Settings.vue"
-import StatusModal from "./components/Modals/StatusModal.vue"
 import ConfigPanel from "./components/Tabs/Config.vue"
 import DashboardPanel from "./components/Tabs/Dashboard.vue"
+
+import StatusModal from "./components/Modals/StatusModal.vue"
 
 export default {
   name: "App",
