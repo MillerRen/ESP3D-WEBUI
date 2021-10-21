81949bc4d8990412c27d5bb5ae98924dd44e4b6b
MillerRen <ren12345@126.com>
2021-10-21 19:09:55 +0800

    temperature panel

--- src/components/DashboardPanel.vue
+++ src/components/DashboardPanel.vue
@@ -8,6 +8,9 @@
                 <GrblPanel />
             </div>
             <div class="col-sm-6">
+                <TemperaturePanel />
+            </div>
+            <div class="col-sm-6">
                 <SDPanel />
             </div>
             <div class="col-sm-6">
@@ -22,13 +25,15 @@ import ControlsPanel from "./ControlsPanel.vue"
 import ConsolePanel from './ConsolePanel.vue'
 import GrblPanel from "./GrblPanel.vue"
 import SDPanel from "./SDPanel.vue"
+import TemperaturePanel from "./TemperaturePanel.vue"
 
 export default {
     components: {
         ControlsPanel,
         ConsolePanel,
         GrblPanel,
-        SDPanel
+        SDPanel,
+        TemperaturePanel
     }
 }
 </script>
