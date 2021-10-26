df8365da437081488ad834f6fbf3c75da04a9c84
MillerRen <ren12345@126.com>
2021-10-26 19:05:25 +0800

    mock delay response

--- mocks/lib/mock.js
+++ mocks/lib/mock.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 import AxiosMockAdapter from 'axios-mock-adapter'
 
-const mock = new AxiosMockAdapter(axios, { onNoMatch: "passthrough" })
+const mock = new AxiosMockAdapter(axios, {
+    onNoMatch: "passthrough",
+    delayResponse: 1000
+})
 
 export default mock
