23cb8956dc3a1c732b9c1bd4a510ce10e11d8486
MillerRen <ren12345@126.com>
2021-11-19 11:23:01 +0800

    。。

--- src/components/Tabs/Config.vue
+++ src/components/Tabs/Config.vue
@@ -4,11 +4,18 @@
       <h2>
         <span v-t>Printer configuration</span>
       </h2>
-      <button type="button" class="btn btn-primary" @click="refreshConfig()">
-        <i class="glyphicon glyphicon-refresh"></i>
-      </button>
-      <br />
-      <span></span>
+      <div class="btn-toolbar" style="display: inline-block">
+        <!-- <button type="button" class="btn btn-success" @click="refreshConfig()">
+          <i class="glyphicon glyphicon-save"></i>
+        </button>
+        <button type="button" class="btn btn-warning" @click="refreshConfig()">
+          <i class="glyphicon glyphicon-trash"></i>
+        </button> -->
+        <button type="button" class="btn btn-primary" @click="refreshConfig()">
+          <i class="glyphicon glyphicon-refresh"></i>
+        </button>
+      </div>
+
       <hr />
       <div class="loader" v-show="loading"></div>
       <div>
@@ -112,60 +119,6 @@
             </tbody>
           </table>
         </div>
-        <!-- <div  class="hidden">
-          <table>
-            <tr>
-              <td style="padding: 10px">
-                <button
-                  type="button"
-                  
-                  class="btn btn-success"
-                  onclick="Apply_config_override()"
-                >
-                  <span>
-                    <svg width="1.3em" height="1.2em" viewBox="0 0 1300 1200">
-                      <g transform="translate(50,1200) scale(1, -1)">
-                        <path
-                          fill="currentColor"
-                          d="M450 1200h200q21 0 35.5 -14.5t14.5 -35.5v-350h245q20 0 25 -11t-9 -26l-383 -426q-14 -15 -33.5 -15t-32.5 15l-379 426q-13 15 -8.5 26t25.5 11h250v350q0 21 14.5 35.5t35.5 14.5zM50 300h1000q21 0 35.5 -14.5t14.5 -35.5v-250h-1100v250q0 21 14.5 35.5t35.5 14.5z M900 200v-50h100v50h-100z"
-                        />
-                      </g>
-                    </svg>
-                  </span>
-                </button>
-              </td>
-              <td style="padding: 10px">
-                <button
-                  type="button"
-                  
-                  class="btn btn-warning"
-                  v-if="fwData.target_firmware=='smoothieware'"
-                >
-                  <span>
-                    <svg width="1.3em" height="1.2em" viewBox="0 0 1300 1200">
-                      <g transform="translate(50,1200) scale(1, -1)">
-                        <path
-                          fill="currentColor"
-                          d="M500 1300h300q41 0 70.5 -29.5t29.5 -70.5v-100h275q10 0 17.5 -7.5t7.5 -17.5v-75h-1100v75q0 10 7.5 17.5t17.5 7.5h275v100q0 41 29.5 70.5t70.5 29.5zM500 1200v-100h300v100h-300zM1100 900v-800q0 -41 -29.5 -70.5t-70.5 -29.5h-700q-41 0 -70.5 29.5t-29.5 70.5 v800h900zM300 800v-700h100v700h-100zM500 800v-700h100v700h-100zM700 800v-700h100v700h-100zM900 800v-700h100v700h-100z"
-                        />
-                      </g>
-                    </svg>
-                  </span>
-                </button>
-              </td>
-            </tr>
-          </table>
-          <table
-            class="table table-bordered table-striped table-hover table-responsive"
-            style="
-              width: auto;
-              max-width: 90% !important;
-              word-wrap: break-word;
-            "
-          >
-            <tbody ></tbody>
-          </table>
-        </div>-->
       </div>
     </center>
   </div>
