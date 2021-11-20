cb1274e25f5bba5553821aaa17ff7b4e5d4cbd12
MillerRen <ren12345@126.com>
2021-11-20 09:23:50 +0800

    mock

--- mocks/commands/index.js
+++ mocks/commands/index.js
@@ -1,11 +1,11 @@
 module.exports = function (app) {
   app.get(
     '/command',
+    require('./ESP220'),
     require('./ESP800'),
     require('./ESP400'),
     require('./❓'),
     require('./$$'),
-    require('../notfound'),
-    require('./ESP220')
+    require('../notfound')
   )
 }
