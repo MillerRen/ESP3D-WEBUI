ef9d0be6c4ff9fdf01c7b82de21517d0b3fdc4df
MillerRen <ren12345@126.com>
2021-11-20 10:37:43 +0800

    mock

--- mocks/commands/ESP420.js
+++ mocks/commands/ESP420.js
@@ -1,10 +1,9 @@
-import mock from '../mock'
 
 var response = "Chip ID: 13874112\nCPU Frequency: 160Mhz\nFree memory: 24.23 KB\nSDK: 2.0.0(656edbf)\nFlash Size: 4.00 MB\nAvailable Size for update: 652.17 KB(Ok)\nAvailable Size for SPIFFS: 3.00 MB\nBaud rate: 115200\nSleep mode: None\nChannel: 1\nPhy Mode: 11g\nWeb port: 80\nData port: 8888\nHostname: lucesp\nActive Mode: Station (5C:CF:7F:D3:B3:C0)\nConnected to: NETGEAR_2GEXT_OFFICE2\nSignal: 98%\nIP Mode: DHCP\nIP: 192.168.1.51\nGateway: 192.168.1.1\nMask: 255.255.255.0\nDNS: 192.168.1.1\nDisabled Mode: Access Point (5E:CF:7F:D3:B3:C0)\nCaptive portal: Enabled\nSSDP: Enabled\nNetBios: Enabled\nmDNS: Enabled\nWeb Update: Enabled\nPin Recovery: Disabled\nAuthentication: Disabled\nTarget Firmware: Smoothieware\nSD Card Support: Enabled\nFW version: 0.9.93\n";
 
-mock.onGet('/command', {
-    params: {
-        plain: '[ESP420]plain'
+module.exports = function (req, res, next) {
+    if(req.query.plain == '[ESP420]plain') {
+        return res.send(response).end()
     }
-})
-.reply(200, response)
+    next()
+}
--- mocks/commands/index.js
+++ mocks/commands/index.js
@@ -4,6 +4,7 @@ module.exports = function (app) {
     require('./ESP220'),
     require('./ESP800'),
     require('./ESP400'),
+    require('./ESP420'),
     require('./❓'),
     require('./$$'),
     require('../notfound')
