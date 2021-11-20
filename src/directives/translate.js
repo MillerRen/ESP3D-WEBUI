0e04b428c8eb0903b5a502fe62bce85b9af6a61e
MillerRen <ren12345@126.com>
2021-11-20 10:25:15 +0800

    translate

--- src/directives/translate.js
+++ src/directives/translate.js
@@ -5,6 +5,7 @@ const messages = require(`../languages/${lang}`).default
 function translate (el, binding) {
   let key = binding.value || el.textContent
   key = key.trim()
+  if(key=='Yes')console.log(key, messages[key])
   el.textContent = messages[key] || key // translate and fallback
 }
 
