2d56d7d3278c4a58de2490f9bead60e893b6f67e
MillerRen <ren12345@126.com>
2021-11-26 09:23:40 +0800

    work position

--- src/models/grbl/index.js
+++ src/models/grbl/index.js
@@ -153,6 +153,14 @@ export default class Grbl {
         : []
       
       this.sd = report.data.sd
+      if(report.data.workcoordinateOffset) {
+        this.WPos.x -= this.WCO.x
+        this.WPos.y -= this.WCO.y
+        this.WPos.z -= this.WCO.z
+        this.WPos.a -= this.WCO.a
+        this.WPos.b -= this.WCO.b
+        this.WPos.c -= this.WCO.c
+      }
       if (report.data.status.state == 'Hold') {
         this.grblErrorMessage =
           report.data.status.state + ':' + report.data.status.code
