fc66d301e9dd2da9a047d953728b009195926b8b
MillerRen <ren12345@126.com>
2021-11-30 23:17:47 +0800

    bugfix

--- src/lib/grbl/middleware/reset.js
+++ src/lib/grbl/middleware/reset.js
@@ -3,6 +3,6 @@ const regex = /^Grbl\sv?(\d\.\d.)\s\['\$'\sfor\shelp\]/
 export default function (ctx, next) {
   if(!regex.test(ctx.input)) return next()
   ctx.type = 'reset'
-  ctx.version = regex.match(ctx.input)[1]
+  ctx.version = ctx.input.match(regex)[1]
   next()
 }
