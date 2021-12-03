2fb0f743439c94aca06527f27cbec1b3253c6e76
MillerRen <ren12345@126.com>
2021-12-03 22:52:06 +0800

    ..

--- src/lib/websocket.js
+++ src/lib/websocket.js
@@ -1,63 +1,63 @@
-if (process.env.NODE_ENV == 'development') {
-    require('../../mocks/websocket')
-}
-
-function noop() {}
+function noop () {}
 
 module.exports = function (url, opts) {
-	opts = opts || {};
+  opts = opts || {}
 
-	var ws, num=0, timer=1, $={};
-	var max = opts.maxAttempts || Infinity;
+  var ws,
+    num = 0,
+    timer = 1,
+    $ = {}
+  var max = opts.maxAttempts || Infinity
 
-	$.open = function () {
-		ws = new WebSocket(url, opts.protocols || []);
+  $.open = function () {
+    ws = new WebSocket(url, opts.protocols || [])
 
-        ws.binaryType = opts.binaryType || 'arraybuffer'
+    ws.binaryType = opts.binaryType || 'arraybuffer'
 
-		ws.onmessage = opts.onmessage || noop;
+    ws.onmessage = opts.onmessage || noop
 
-		ws.onopen = function (e) {
-			(opts.onopen || noop)(e);
-			num = 0;
-		};
+    ws.onopen = function (e) {
+      (opts.onopen || noop)(e)
+      num = 0
+    }
 
-		ws.onclose = function (e) {
-			e.code === 1e3 || e.code === 1001 || e.code === 1005 || $.reconnect(e);
-			(opts.onclose || noop)(e);
-		};
+    ws.onclose = function (e) {
+      e.code === 1e3 || e.code === 1001 || e.code === 1005 || $.reconnect(e)
+      ;(opts.onclose || noop)(e)
+    }
 
-		ws.onerror = function (e) {
-			(e && e.code==='ECONNREFUSED') ? $.reconnect(e) : (opts.onerror || noop)(e);
-		};
-	};
+    ws.onerror = function (e) {
+      e && e.code === 'ECONNREFUSED'
+        ? $.reconnect(e)
+        : (opts.onerror || noop)(e)
+    }
+  }
 
-	$.reconnect = function (e) {
-		if (timer && num++ < max) {
-			timer = setTimeout(function () {
-				(opts.onreconnect || noop)(e);
-				$.open();
-			}, opts.timeout || 1e3);
-		} else {
-			(opts.onmaximum || noop)(e);
-		}
-	};
+  $.reconnect = function (e) {
+    if (timer && num++ < max) {
+      timer = setTimeout(function () {
+        (opts.onreconnect || noop)(e)
+        $.open()
+      }, opts.timeout || 1e3)
+    } else {
+      (opts.onmaximum || noop)(e)
+    }
+  }
 
-	$.json = function (x) {
-		ws.send(JSON.stringify(x));
-	};
+  $.json = function (x) {
+    ws.send(JSON.stringify(x))
+  }
 
-	$.send = function (x) {
-		ws.send(x);
-	};
+  $.send = function (x) {
+    ws.send(x)
+  }
 
-	$.close = function (x, y) {
-		timer = clearTimeout(timer);
-		ws.close(x || 1e3, y);
-	};
+  $.close = function (x, y) {
+    timer = clearTimeout(timer)
+    ws.close(x || 1e3, y)
+  }
 
-	$.open(); // init
+  $.open() 
 
-	return $;
+  return $
 }
-
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import './directives'
-import './components'
 import './plugins'
 
 import App from './App.vue'
