a9b8c5129d8dd05ca2886a6aa4b86d70f805a60e
MillerRen <ren12345@126.com>
2021-12-04 23:18:21 +0800

    ...

--- src/components/Tabs/Dashboard.vue
+++ src/components/Tabs/Dashboard.vue
@@ -3,7 +3,7 @@
     <div class="grid-container">
       <template v-for="panel in panels">
         <component
-          v-if="preferences[panel.enable]=='true'"
+          v-if="preferences[panel.enable] == 'true'&&panel.show"
           :key="panel.panel"
           :is="panel.panel"
         ></component>
@@ -19,26 +19,38 @@ export default {
     return {
       panels: [
         {
+          show: true,
           panel: "ControlsPanel",
           enable: "enable_control_panel",
         },
         {
+          show:
+            this.fwData.target_firmware == "grbl" ||
+            this.fwData.target_firmware == "grbl-embedded",
           panel: "GrblPanel",
           enable: "enable_grbl_panel",
         },
         {
+          show:
+            this.fwData.target_firmware == "marlin" ||
+            this.fwData.target_firmware == "marlin-embedded",
           panel: "TemperaturePanel",
           enable: "enable_temperatures_panel",
         },
         {
+          show:
+            this.fwData.target_firmware == "marlin" ||
+            this.fwData.target_firmware == "marlin-embedded",
           panel: "ExtruderPanel",
           enable: "enable_extruder_panel",
         },
         {
+          show: true,
           panel: "SDPanel",
           enable: "enable_files_panel",
         },
         {
+          show: true,
           panel: "ConsolePanel",
           enable: "enable_commands_panel",
         },
