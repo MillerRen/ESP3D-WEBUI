d28e4b3c18025022f9c4fe438ed8a9a5997ca0df
MillerRen <ren12345@126.com>
2021-12-05 20:02:52 +0800

    ..

--- src/directives/translate.js
+++ src/directives/translate.js
@@ -1,11 +1,8 @@
 // simple translate directive
-import Vue from 'vue'
-// import messages from '../languages/zh_CN'
+import messages from '../assets/languages/zh_CN'
 
-Vue.prototype.$messages = {}
-
-export default function (el, binding, vnode) {
+export default function (el, binding) {
   let key = binding.value || el.textContent
   key = key.trim()
-  el.textContent = vnode.context.$messages[key] || key // translate and fallback
+  el.textContent = messages[key] || key // translate and fallback
 }
